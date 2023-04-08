da789f988952821a9f23ebae55d3ac6608a4df35
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-04-08 22:53:58 +0800

    create PageManage
    add Manage Page

--- src/views/PageManage.vue
+++ src/views/PageManage.vue
@@ -1,28 +1,78 @@
 <template>
     <div>
-
+      <el-table
+          :data="userList"
+          :default-sort="{ prop: 'date', order: 'descending' }"
+          style="width: 100%"
+      >
+        <el-table-column prop="date" label="Date" sortable width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address"/>
+        <el-table-column label="Operations">
+          <template #default="scope">
+            <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+            >修改</el-button
+            >
+            <el-button
+                size="small"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
 </template>
-<script>
+<script setup>
 import config from '../config'
+import {ref} from 'vue'
 
-export default {
-    data()
-    {
-        return {
-            title: '',
-            owner: '',
-            description: ''
-        };
-    },
-    methods: {
-        submitForm()
-        {
-            this.axios.post(config.API_LIVE_CREATE + '?title=' + this.title + '&description=' + this.description + '&owner=' + this.owner).then((response) =>
-            {
-                this.$router.push('/')
-            })
-        }
-    }
+const title = ref('')
+const owner = ref('')
+const description = ref('')
+const userList = ref([])
+
+onload(()=>{
+  getAllUser()
+})
+
+const getAllUser = ()=>{
+    //getUserList
+    let Users = [
+      {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+    ]
+  this.userList = Users
 }
+
+const handleEdit = (index,row)=>{
+  console.log("index",index + " row:",row)
+  //row Full Object
+  //index: current index
+}
+const handleDelete = (index,row)=>{
+  console.log("index",index + " row:",row)
+  //row Full Object
+  //index: current index
+}
+
 </script>
