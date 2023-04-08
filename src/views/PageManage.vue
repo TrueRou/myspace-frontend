4cad2ae4f552246821345264e2aa809408c4739c
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-04-08 23:25:36 +0800

    updata ManagePage
    change view

--- src/views/PageManage.vue
+++ src/views/PageManage.vue
@@ -1,30 +1,48 @@
 <template>
-  <div>
-    <el-table :data="userList" :default-sort="{ prop: 'date', order: 'descending' }" style="width: 100%">
-      <el-table-column prop="date" label="Date" sortable width="180" />
-      <el-table-column prop="name" label="Name" width="180" />
-      <el-table-column prop="address" label="Address" />
-      <el-table-column label="Operations">
-        <template #default="scope">
-          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">修改</el-button>
-          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <el-table
+              :data="userList"
+              :border="true"
+              :default-sort="{ prop: 'date', order: 'descending' }"
+              style="width: 100%"
+          >
+            <el-table-column prop="id" label="id" sortable width="180" />
+            <el-table-column prop="username" label="用户名"/>
+            <el-table-column prop="email" label="邮箱" width="180" />
+            <el-table-column prop="is_superuser" label="是否为超级用户"/>
+            <el-table-column label="Operations">
+              <template #default="scope">
+                <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+                >修改</el-button
+                >
+                <el-button
+                    size="small"
+                    type="danger"
+                    @click="handleDelete(scope.$index, scope.row)"
+                >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script setup>
 import config from '../config'
 import { onMounted, ref } from 'vue'
 import axios from 'axios';
-
 const userList = ref([])
-
 onMounted(() =>
 {
   getAllUser()
 })
-
 const getAllUser = async () =>
 {
   const token = localStorage.getItem('token')
@@ -32,7 +50,6 @@ const getAllUser = async () =>
   userList.value = response.data
   console.log(userList)
 }
-
 const handleEdit = async (object) =>
 {
   const token = localStorage.getItem('token')
@@ -48,5 +65,4 @@ const handleDelete = async (object) =>
   const id = object.id
   await axios.delete(config.API_USER + `/${id}`, body, options)
 }
-
 </script>
