3d29c2507d5180fdf226eef7a1a0a1d92c5ba041
Usagi no Niku <chenbohan911@163.com>
2023-04-09 10:42:53 +0800

    normalize theme

--- src/views/PageLogin.vue
+++ src/views/PageLogin.vue
@@ -1,46 +1,19 @@
 <template>
-  <div class="bg">
-    <div class="body">
-      <div class="login-form">
-        <el-form label-position="top" label-width="100px" class="demo-ruleForm" :rules="rules" :model="formContent"
-          status-icon ref="loginForm">
-          <el-form-item prop="email">
-            <div class="inputBox">
-              <input type="text" v-model="formContent.email" required="required">
-              <span>邮箱</span>
-              <i></i>
-            </div>
-          </el-form-item>
-          <el-form-item prop="password">
-            <div class="inputBox">
-              <input type="text" v-model="formContent.password" required="required">
-              <span>密码</span>
-              <i></i>
-            </div>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width: 100%" type="primary" @click="submitForm()">登录</el-button>
-          </el-form-item>
-          <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>
-        </el-form>
-      </div>
-    </div>
+  <div class="body bg">
+    <el-form class="login-form" label-position="top" label-width="100px" :rules="rules" :model="formContent" status-icon
+      ref="loginForm">
+      <el-form-item class="form-item" label="邮箱" prop="email">
+        <el-input class="input-box" type="text" v-model="formContent.email"></el-input>
+      </el-form-item>
+      <el-form-item class="form-item" label="密码" prop="password">
+        <el-input class="input-box" type="password" v-model="formContent.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button style="width: 100%" type="primary" @click="submitForm()">登录</el-button>
+      </el-form-item>
+      <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>
+    </el-form>
   </div>
-  <!--  <div style="background-color: white;" class="login-section login-card">-->
-  <!--    <el-form label-position="top" label-width="100px" class="demo-ruleForm" :rules="rules" :model="rulesForm"-->
-  <!--             status-icon ref="ruleForm">-->
-  <!--      <el-form-item label="邮箱" prop="email">-->
-  <!--        <el-input type="text" v-model="rulesForm.email"></el-input>-->
-  <!--      </el-form-item>-->
-  <!--      <el-form-item label="密码" prop="password">-->
-  <!--        <el-input type="password" v-model="rulesForm.password"></el-input>-->
-  <!--      </el-form-item>-->
-  <!--      <el-form-item>-->
-  <!--        <el-button style="width: 100%" type="primary" @click="submitForm('ruleForm')">登录</el-button>-->
-  <!--      </el-form-item>-->
-  <!--      <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>-->
-  <!--    </el-form>-->
-  <!--  </div>-->
 </template>
 <script setup>
 import config from '../config'
@@ -99,6 +72,10 @@ const submitForm = async () =>
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 }
 
+.form-item .el-form-item__label {
+  color: white;
+}
+
 * {
   margin: 0;
   padding: 0;
@@ -121,76 +98,14 @@ const submitForm = async () =>
   gap: 30px;
 }
 
-.inputBox {
-  position: relative;
-  width: 250px;
-  margin-bottom: 20px;
-}
-
-.inputBox input {
-  width: 100%;
+.input-box .el-input__wrapper {
   background: transparent;
-  color: #fff;
-  border: none;
-  outline: none;
-  box-shadow: none;
-  font-size: 1em;
-  letter-spacing: 0.1em;
-  padding: 10px 0 5px;
+  width: 250px;
+  border: gray;
 }
 
-.inputBox span {
-  position: absolute;
-  left: 0;
-  color: rgba(255, 255, 255, 0.5);
-  text-transform: uppercase;
-  pointer-events: none;
-  letter-spacing: 0.1em;
-  transition: 0.5s;
-  font-size: 1.4em;
-}
-
-.inputBox input:valid~span,
-.inputBox input:focus~span {
-  color: #2196f3;
-  transform: translateY(-16px);
-  font-size: 1em;
-}
-
-.inputBox i {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 2px;
-  background: #fff;
-  overflow: hidden;
-}
-
-.inputBox i::before {
-  content: '';
-  position: absolute;
-  left: -100%;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(90deg, #ff1b69, #ff0, #2196f3, #9c27b0, #ff1b69);
-  animation: animate 2s linear infinite;
-  transition: 0.5s;
-}
-
-.inputBox input:valid~i::before,
-.inputBox input:focus~i::before {
-  left: 0;
-}
-
-@keyframes animate {
-  0% {
-    background-position-x: 0;
-  }
-
-  100% {
-    background-position-x: 250px;
-  }
+.input-box .el-input__inner {
+  color: white;
 }
 </style>
 
