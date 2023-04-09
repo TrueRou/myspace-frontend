c9dccace94138b73bbf637d1b35c86df18d13f02
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-04-09 09:46:18 +0800

    updata PageLogin.vue
    remake login page,wait to fix scripts bug

--- src/views/PageLogin.vue
+++ src/views/PageLogin.vue
@@ -1,89 +1,188 @@
 <template>
-    <div style="background-color: white;" class="login-section login-card">
+  <div class="bg">
+    <div class="body">
+      <div class="login-form">
         <el-form label-position="top" label-width="100px" class="demo-ruleForm" :rules="rules" :model="rulesForm"
-            status-icon ref="ruleForm">
-            <el-form-item label="邮箱" prop="email">
-                <el-input type="text" v-model="rulesForm.email"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-                <el-input type="password" v-model="rulesForm.password"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button style="width: 100%" type="primary" @click="submitForm('ruleForm')">登录</el-button>
-            </el-form-item>
-            <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>
+                 status-icon ref="ruleForm">
+          <el-form-item prop="email">
+            <div class="inputBox">
+              <input type="text" required="required">
+              <span>邮箱</span>
+              <i></i>
+            </div>
+          </el-form-item>
+          <el-form-item prop="password">
+            <div class="inputBox">
+              <input type="text" required="required">
+              <span>密码</span>
+              <i></i>
+            </div>
+          </el-form-item>
+          <el-form-item>
+            <el-button style="width: 100%" type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          </el-form-item>
+          <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>
         </el-form>
+      </div>
     </div>
+  </div>
+  <!--  <div style="background-color: white;" class="login-section login-card">-->
+  <!--    <el-form label-position="top" label-width="100px" class="demo-ruleForm" :rules="rules" :model="rulesForm"-->
+  <!--             status-icon ref="ruleForm">-->
+  <!--      <el-form-item label="邮箱" prop="email">-->
+  <!--        <el-input type="text" v-model="rulesForm.email"></el-input>-->
+  <!--      </el-form-item>-->
+  <!--      <el-form-item label="密码" prop="password">-->
+  <!--        <el-input type="password" v-model="rulesForm.password"></el-input>-->
+  <!--      </el-form-item>-->
+  <!--      <el-form-item>-->
+  <!--        <el-button style="width: 100%" type="primary" @click="submitForm('ruleForm')">登录</el-button>-->
+  <!--      </el-form-item>-->
+  <!--      <el-tag v-if="error" class="ml-2" type="danger">登录失败, 可能是邮箱或密码错误</el-tag>-->
+  <!--    </el-form>-->
+  <!--  </div>-->
 </template>
-<script>
+<script setup>
 import config from '../config'
 import Qs from 'qs';
-import { useUserStore } from '../stores/UserStore';
+import {useUserStore} from '../stores/UserStore';
+import {onMounted, ref} from "vue";
 
-export default {
-    data()
-    {
-        return {
-            error: false,
-            rulesForm: {
-                email: '',
-                password: ''
-            },
-            rules: {
-                email: [
-                    { required: true, message: '请填写邮箱', trigger: 'blur' },
-                ],
-                password: [
-                    { required: true, message: '请填写密码', trigger: 'blur' },
-                    { min: 5, max: 12, message: '密码的长度范围为5-12', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-    methods: {
-        async submitForm(formName)
-        {
-            const userStore = useUserStore()
+const userStore = useUserStore()
+const rulesForm = {email: '', password: ''}
+const rules = {email: [{required: true, message: '请填写邮箱', trigger: 'blur'},],
+  password: [{required: true, message: '请填写密码', trigger: 'blur'}, {
+    min: 5,
+    max: 12,
+    message: '密码的长度范围为5-12',
+    trigger: 'blur'
+  }]
+}
+let error = ref(false)
 
-            var params = Qs.stringify({
-                'username': this.rulesForm.email,
-                'password': this.rulesForm.password
-            })
 
-            this.$refs[formName].validate(async (valid) =>
-            {
-                if (valid)
-                {
-                    try
-                    {
-                        const response = await this.axios.post(config.API_USER_LOGIN, params)
-                        localStorage.setItem('token', response.data['access_token']);
-                        await userStore.findUser()
-                        location.href = '/'
-                    } catch (error)
-                    {
-                        console.log(error.response.data.error)
-                        this.error = true
-                    }
-                }
-            });
-        }
+const submitForm = async (formName) => {
+  var params = Qs.stringify({
+    'username': rulesForm.email,
+    'password': rulesForm.password
+  })
+  this.$refs[formName].validate(async (valid) => {
+    if (valid) {
+      try {
+        const response = await this.axios.post(config.API_USER_LOGIN, params)
+        localStorage.setItem('token', response.data['access_token']);
+        await userStore.findUser()
+        location.href = '/'
+      } catch (e) {
+        console.log(e.response.data.error)
+        error = true
+      }
     }
+  });
 }
 </script>
-   
-   
 <style>
-.login-section {
-    padding: 0px 20px
+.login-form {
+  background: rgb(0, 0, 0, 0.5);
+  opacity: 1;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 }
 
-.login-card {
-    position: absolute;
-    width: 300px;
-    height: 400px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+.bg {
+  background-size: cover;
+  /*也许之后会改成随机url？*/
+  background-image: url(https://img.paulzzh.tech/touhou/random);
+}
+
+.body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.inputBox {
+  position: relative;
+  width: 250px;
+  margin-bottom: 20px;
+}
+
+.inputBox input {
+  width: 100%;
+  background: transparent;
+  color: #fff;
+  border: none;
+  outline: none;
+  box-shadow: none;
+  font-size: 1em;
+  letter-spacing: 0.1em;
+  padding: 10px 0 5px;
+}
+
+.inputBox span {
+  position: absolute;
+  left: 0;
+  color: rgba(255, 255, 255, 0.5);
+  text-transform: uppercase;
+  pointer-events: none;
+  letter-spacing: 0.1em;
+  transition: 0.5s;
+  font-size: 1.4em;
+}
+
+.inputBox input:valid ~ span,
+.inputBox input:focus ~ span {
+  color: #2196f3;
+  transform: translateY(-16px);
+  font-size: 1em;
+}
+
+.inputBox i {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  background: #fff;
+  overflow: hidden;
+}
+
+.inputBox i::before {
+  content: '';
+  position: absolute;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, #ff1b69, #ff0, #2196f3, #9c27b0, #ff1b69);
+  animation: animate 2s linear infinite;
+  transition: 0.5s;
+}
+
+.inputBox input:valid ~ i::before,
+.inputBox input:focus ~ i::before {
+  left: 0;
+}
+
+@keyframes animate {
+  0% {
+    background-position-x: 0;
+  }
+
+  100% {
+    background-position-x: 250px;
+  }
 }
 </style>
+
+
